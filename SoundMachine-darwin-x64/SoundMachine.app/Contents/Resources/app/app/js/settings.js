4d2f5c30885ef89cdd13ea97c1e774fcc02ec3ca
Mark Morris <mmorris@eir.local>
2016-09-29 09:20:22 -0600

    Removed output file

--- SoundMachine-darwin-x64/SoundMachine.app/Contents/Resources/app/app/js/settings.js
+++
@@ -1,7 +0,0 @@
-const { ipcRenderer } = require('electron');
-
-const closeEl = document.querySelector('.close');
-
-closeEl.addEventListener('click', function() {
-  ipcRenderer.send('close-settings-window');
-});
